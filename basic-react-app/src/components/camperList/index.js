e6315a2364084db607a16c6ff46cf0f244034ff7
acochran <aaron.cochran@safecu.org>
2017-10-24 16:37:16 -0700

    20171024

--- basic-react-app/src/components/camperList/index.js
+++ basic-react-app/src/components/camperList/index.js
@@ -1,61 +1,74 @@
-import React, {  Component } from 'react'
-import { Header, Menu, Container, Button } from 'semantic-ui-react'
-import { Image, Table } from 'semantic-ui-react'
+import React, {Component} from 'react'
+import {Header, Menu, Container, Button} from 'semantic-ui-react'
+import {Image, Table} from 'semantic-ui-react'
 
 
 class CamperList extends Component {
-   state = { campers: []}
-   async componentDidMount() {
+    state = {campers: [], ascOrder: 1}
 
-    const promise = await fetch('https://fcctop100.herokuapp.com/api/fccusers/top/recent')
+    async componentDidMount() {
 
-      const stuff = await promise.json()
+        const promise = await fetch('https://fcctop100.herokuapp.com/api/fccusers/top/recent')
 
-      console.log(stuff)
-      this.setState({campers: stuff })
+        const stuff = await promise.json()
+
+        console.log(stuff)
+        this.setState({campers: stuff})
 
 
-  }
-  render () {
-    const {campers} = this.state
-    return (<div>
-        <Table basic='very' celled collapsing>
-          <Table.Header>
-            <Table.Row>
-              <Table.HeaderCell>Users</Table.HeaderCell>
-              <Table.HeaderCell></Table.HeaderCell>
-            </Table.Row>
-          </Table.Header>
+    }
 
-          <Table.Body>
+    handleHeaderClick = (sortBy) => {
+        if (sortBy === this.state.sortBy) {
+            this.setState({ascOrder: -1 * this.state.ascOrder})
+        }
+        else {
+            this.setState({ascOrder: 1, sortBy})
+        }
+    }
 
-            { campers.sort( (camper1, camper2) => {
-               //return camper1.alltime - camper2.alltime
-              return new Date(camper1.lastUpdate) > new Date(camper2.lastUpdate) ? -1 : 1
-            }).reverse().map((camper) => {
-            return <Table.Row key={camper.id}>
-              <Table.Cell>
-                <Header as='h4' image>
-                  <Image src={camper.img} shape='rounded' size='mini' />
-                  <Header.Content>
-                    {camper.username}
-                    <Header.Subheader>{camper.alltime}</Header.Subheader>
-                  </Header.Content>
-                </Header>
-              </Table.Cell>
-              <Table.Cell>
-                {camper.recent}
-              </Table.Cell>
-              <Table.Cell>
-                {camper.lastUpdate}
-              </Table.Cell>
-            </Table.Row>
-            })}
-          </Table.Body>
-        </Table>
-      </div>
-    )
-  }
+    render() {
+        const {campers, sortBy, ascOrder} = this.state
+        return (<div>
+                <Table basic='very' celled collapsing>
+                    <Table.Header>
+                        <Table.Row>
+                            <Table.HeaderCell
+                                onClick={() => this.handleHeaderClick('username')}>Users</Table.HeaderCell>
+                            <Table.HeaderCell onClick={() => this.handleHeaderClick('alltime')}>All
+                                Time</Table.HeaderCell>
+                        </Table.Row>
+                    </Table.Header>
+
+                    <Table.Body>
+
+                        {campers.sort((camper1, camper2) => {
+                            //return camper1.alltime - camper2.alltime
+                            return ascOrder*(camper1[sortBy] > camper2[sortBy] ? -1 : 1)
+                        }).map((camper) => {
+                            return <Table.Row key={camper.username}>
+                                <Table.Cell>
+                                    <Header as='h4' image>
+                                        <Image src={camper.img} shape='rounded' size='mini'/>
+                                        <Header.Content>
+                                            {camper.username}
+                                            <Header.Subheader>{camper.alltime}</Header.Subheader>
+                                        </Header.Content>
+                                    </Header>
+                                </Table.Cell>
+                                <Table.Cell>
+                                    {camper.recent}
+                                </Table.Cell>
+                                <Table.Cell>
+                                    {camper.lastUpdate}
+                                </Table.Cell>
+                            </Table.Row>
+                        })}
+                    </Table.Body>
+                </Table>
+            </div>
+        )
+    }
 }
 
 export default CamperList
